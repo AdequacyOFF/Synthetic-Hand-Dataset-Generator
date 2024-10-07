ed6e4dff9438f3af1b85e399e294e83047f0aa81
ADoff <161216087+AdequacyOFF@users.noreply.github.com>
2024-10-07 23:25:27 +0300

    add zip create and download

--- CServer/Program.cs
+++ CServer/Program.cs
@@ -1,17 +1,27 @@
 
-using Grpc.Net.Client;
 using GrpcHandGeneratorClient;
 using CServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+        {
+            // this defines a CORS policy called "default"
+            options.AddPolicy("default", policy =>
+            {
+                policy.WithOrigins("http://localhost:5173")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });  
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
 
+var app = builder.Build();
+app.UseCors("default");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -22,19 +32,23 @@ if (app.Environment.IsDevelopment())
 app.MapPost("/generate-hand-dataset", async (HttpRequest request) =>
 {
     var json = await request.ReadFromJsonAsync<WebHandRequest>();
+    Console.WriteLine($"Generating {json.count} hands for race {json.race} and hand {json.hand}");
     bool raceIsValid = Enum.IsDefined(typeof(Races), json.race);
     bool handIsValid = Enum.IsDefined(typeof(Hand), json.hand);
     if (json.count > 0 && handIsValid && raceIsValid)
     {
         HandDatasetHandler handler = new(json.count, (Races)Enum.Parse(typeof(Races), json.race), (Hand)Enum.Parse(typeof(Hand), json.hand));
-        return Results.Ok(await handler.GetGeneratedHandDataset());
+            string datasetPath = await handler.GetGeneratedHandDataset();
+            return Results.File(datasetPath);
     }
     else
     {
         return Results.BadRequest("Invalid request parameters");
     }
-}).WithName("PostGenerateHandDataset")
+})
+.WithName("PostGenerateHandDataset")
 .WithOpenApi();
+
 
 app.Run("http://localhost:5020");
 
